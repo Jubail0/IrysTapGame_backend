76c3ddce1a8f545af3554eb99e91df6e5db7229e
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2025-11-12 15:08:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import nftRoutes from "./routes/nftRoutes.js";
@@ -9,14 +10,15 @@ import publicRoutes from "./routes/publicRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import bodyParser from "body-parser";
 
-dotenv.config();
 await connectDB();
 
 const app = express();
 app.use(cors({
   origin: process.env.FRONT_END_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
 app.use(express.json());
 app.use(bodyParser.json());
 
