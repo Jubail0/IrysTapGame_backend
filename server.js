134797a201a8ce80ca4fd76901e56f9729a3c1c3
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2025-11-13 15:54:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ await connectDB();
 
 const app = express();
 app.use(cors({
-  origin: process.env.FRONT_END_URL ||"https://tap-royale-backend.onrender.com" ,
+  origin: process.env.FRONT_END_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
